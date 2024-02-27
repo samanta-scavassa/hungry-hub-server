6152743c41128998407ebbb4a4f5e3c0458fad93
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-02-27 21:14:10 +0100

    user authentication

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require(`./db`);
 const express = require("express");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 require("./config")(app);
+
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
+
+require("./error-handling")(app);
 
 module.exports = app;
