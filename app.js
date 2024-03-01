c32b2950109597e3d237c1fe953832fec510fe3d
Juan I. Solis <solid.dogtraining@gmail.com>
2024-03-01 17:36:10 +0100

    review model and endpoints

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = express();
 require("./config")(app);
 const addressRouter = require("./routes/address.routes");
 const userRouter = require("./routes/user.routes");
+const reviewRouter = require("./routes/review.routes");
 
 app.use("/api", addressRouter);
 app.use("/api", userRouter);
+app.use("/api", reviewRouter);
 
 const authRouter = require("./routes/auth.routes");
 app.use("/auth", authRouter);
---
+++ models/Review.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  restaurantId: {
+    type: Schema.Types.ObjectId,
+    ref: "Restaurant",
+    required: true,
+  },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  comment: { type: String, required: true, minLength: 20, maxLength: 250 },
+  createdOn: { type: Date, required: true, default: Date.now },
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
