e1bdc5e1188992b9add0862bc83e2d4381476f7e
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-03-04 11:24:39 +0100

    driver fixes

--- models/Driver.model.js
+++ models/Driver.model.js
@@ -2,17 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const driverSchema = new Schema({
-  name: { type: String, required: true },
-  lastName: { type: String, required: true },
-  phoneNumber: { type: String },
+  userId: { type: Schema.Types.ObjectId, ref:"user",  required: true},
   availabilityStatus: {
     type: String,
     enum: ["available", "unavailable"],
     default: "available",
-  },
-  email: { type: String, required: true, unique: true },
-  createdOn: { type: Date, default: Date.now },
-  isActive: { type: Boolean, default: true },
+  }
 });
 
 const Driver = mongoose.model("Driver", driverSchema);
